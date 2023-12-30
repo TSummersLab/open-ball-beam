f66059b968e803d531f2cb2498efbbccba80789d
Ben <42848659+BenGravell@users.noreply.github.com>
2023-12-30 09:39:13 -0500

    use pygit to get repo root programmatically

--- ballbeam/static.py
+++ ballbeam/static.py
@@ -2,11 +2,13 @@
 
 from pathlib import Path
 
-# REPLACE THIS WITH THE PATH TO YOUR ballbeam
-BALLBEAM_PATH = Path("C:/Users/bjgra/GitHub/open-ball-beam/ballbeam")
+import pygit2  # type: ignore[import]
+
+ROOT_PATH = Path(pygit2.Repository(Path.cwd()).workdir)
+
+BALLBEAM_PATH = ROOT_PATH.joinpath("ballbeam")
 
 COMMON_PATH = BALLBEAM_PATH.joinpath("common")
-
 CONFIGURATORS_PATH = BALLBEAM_PATH.joinpath("configurators")
 CALIBRATORS_PATH = BALLBEAM_PATH.joinpath("calibrators")
 CONFIGURATION_PATH = BALLBEAM_PATH.joinpath("configuration")
