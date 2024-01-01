d953ae562b6768d5c4bd40c690f35e6b53b16911
Ben <42848659+BenGravell@users.noreply.github.com>
2024-01-01 13:22:34 -0500

    mypy fix to main.py

--- ballbeam/main.py
+++ ballbeam/main.py
@@ -2,10 +2,17 @@
 
 This script calls the configurators as well as the Python interface.
 
-Use this instead of running interface.py directly to ensure configurators have been run & changed configuration parameters are updated.
+Use this instead of running interface.py directly to ensure configurators have
+been run & changed configuration parameters are updated.
 """
 
-from ballbeam.static import COMMON_PATH, CONFIGURATORS_PATH
+from pathlib import Path
 
-exec(open(CONFIGURATORS_PATH.joinpath("configurators.py")).read())
-exec(open(COMMON_PATH.joinpath("interface.py")).read())
+from ballbeam.configurators.configurators import configure_calibrate_all
+from ballbeam.static import COMMON_PATH
+
+configure_calibrate_all()
+
+# TODO(bgravell): eliminate use of exec() by refactoring interface.py to not use globals  # noqa: FIX002, TD003
+with Path(COMMON_PATH.joinpath("interface.py")).open() as f:
+    exec(f.read())  # noqa: S102
